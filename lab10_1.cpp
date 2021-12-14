efc9b0a6bd2acb232e3588bb90f282f3ebdfedc8
Non <Kitanon_san@cmu.ac.th>
2021-12-14 16:23:54 +0700

    Lab10_1 Finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 int main(){	
+	double loan,rate,ppy;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin >> ppy;
+	
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +20,30 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+	double PrevBalance,Interest,Total,Payment,NewBalance,multiple = rate/100.00;
+	int EndOfYear = 1;
+	PrevBalance = loan;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	do{
+
+	Interest = PrevBalance*multiple;
+	Total = PrevBalance + Interest;
+	Payment = ppy;
+	if (Total-Payment < 0){
+		Payment = Total;
+	}
+	NewBalance = Total-Payment;
 	
+	cout << setw(13) << left << EndOfYear; 
+	cout << setw(13) << left << PrevBalance;
+	cout << setw(13) << left << Interest;
+	cout << setw(13) << left << Total;
+	cout << setw(13) << left << Payment;
+	cout << setw(13) << left << NewBalance;
+	cout << "\n";	
+
+	PrevBalance = NewBalance;
+	EndOfYear ++;
+	} while(NewBalance != 0);
 	return 0;
 }
